404157c522c4bcb2a9daa261385085b751262db6
Prashanth <janagamap@gmail.com>
2015-10-05 14:10:13 -0400

    finished the weather app.
    Split app.js to controllers, routers, directives and services.

--- Starter/app.js
+++ Starter/app.js
@@ -1,27 +1,8 @@
 // Module
 var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
-weatherApp.config(function($routeProvider){
-   $routeProvider
-   .when('/', {
-      templateUrl: 'pages/home.htm',
-      controller: 'homeController'
-    })
-   .when('/forecast', {
-      templateUrl: 'pages/forecast.htm',
-      controller: 'forecastController'
-    })
-
-});
-
-//controllers
-weatherApp.controller('homeController', ['$scope', function($scope){
-    
-    $scope.name = "Prashanth";
-
-}]);
 
 
-weatherApp.controller('forecastController', ['$scope', function($scope){
 
-}]);
+
+
